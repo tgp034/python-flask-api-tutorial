f1e6b82719ffc51d09f539624e17d270d1fd9ac0
Tito Julio González Padial <72378065+tgp034@users.noreply.github.com>
2025-07-21 09:39:20 +0000

    Problems with post and codespace

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    # You can convert that variable into a json string like this
+    json_text = jsonify(todos)
+
+    # And then you can return it to the front end in the response body like this
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    return 'Response for the POST todo'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
